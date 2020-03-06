6d908f2069dbfca98818a488fcea011442155055
Filip <filip.k.wanat@gmail.com>
2020-03-06 18:00:51 +0100

    Add dbContext with mock data

--- IntroLibrary.Core/Models/Book.cs
+++ IntroLibrary.Core/Models/Book.cs
@@ -6,11 +6,15 @@ namespace IntroLibrary.Core.Models
     public class Book
     {
         public int ID { get; set; }
+
         [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
+
         [Required(ErrorMessage = "Author is required")]
         public string Author { get; set; }
+
         [Required(ErrorMessage = "Release Date is required")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
     }
 }
---
+++ IntroLibrary.Core/Models/dbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroLibrary.Core.Models
+{
+    public class dbContext
+    {
+        public dbContext()
+        {
+            InitializeDatabase();
+        }
+
+        public virtual List<Book> Books { get; set ; }
+
+        private void InitializeDatabase()
+        {
+            Books = new List<Book>
+            {
+                new Book
+                {
+                    ID = 1, Author = "Jules Verne", Title = "Twenty Thousand Leagues Under the Sea",
+                    ReleaseDate = new DateTime(1992, 04, 15)
+                },
+                new Book
+                {
+                    ID = 2, Author = "Jules Verne", Title = "Journey to the Center of the Earth",
+                    ReleaseDate = new DateTime(1993, 11, 01)
+                }
+            };
+        }
+
+    }
+}
