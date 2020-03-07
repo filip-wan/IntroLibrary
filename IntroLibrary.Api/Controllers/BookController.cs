14de736738d49e553382bb11e3dd9b7b3b39b7f6
Filip <filip.k.wanat@gmail.com>
2020-03-07 09:46:24 +0100

    Create empty Book Controller

---
+++ IntroLibrary.Api/Controllers/BookController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IntroLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+
+        private readonly ILogger<BookController> _logger;
+
+        public BookController(ILogger<BookController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Post()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public IEnumerable<WeatherForecast> Put()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public IEnumerable<WeatherForecast> Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
